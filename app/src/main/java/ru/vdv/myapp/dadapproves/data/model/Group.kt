9a45d3e76ce7484670b0a4d647711f106ec86982
Dmitriy <stdimensiy@yandex.ru>
2021-09-10 00:07:50 +0300

    Проект "Папа одобряет" / retrofit & room/ изм.3.8
    - Реализовано применением оператора zip для объединения данных из разных источников (репозиторий один запросы разные). Получилось, коротко и удобно.
    - начат переход от жесткой символьно йпривязки (волщебные значчения в параметрах функций) к объектам. Внедрен enem содержащий категории, на чати функций исправил параметры и реализацию.

---
+++ app/src/main/java/ru/vdv/myapp/dadapproves/data/model/Group.kt
@@ -0,0 +1,8 @@
+package ru.vdv.myapp.dadapproves.data.model
+
+enum class Group(val id: Int) {
+    ANECDOTES(1),
+    STORIES(2),
+    POEMS(3),
+    APHORISMS(4)
+}
--- app/src/main/java/ru/vdv/myapp/dadapproves/presentation/dadsoffice/DadsOfficePresenter.kt
+++ app/src/main/java/ru/vdv/myapp/dadapproves/presentation/dadsoffice/DadsOfficePresenter.kt
@@ -1,19 +1,84 @@
 package ru.vdv.myapp.dadapproves.presentation.dadsoffice
 
+import android.util.Log
 import com.github.terrakok.cicerone.Router
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.plusAssign
 import moxy.MvpPresenter
 import ru.vdv.myapp.dadapproves.AndroidScreens
+import ru.vdv.myapp.dadapproves.data.model.Group
+import ru.vdv.myapp.dadapproves.data.model.JokesRepository
 import ru.vdv.myapp.dadapproves.myschedulers.IMySchedulers
-import ru.vdv.myapp.dadapproves.presentation.interfaces.MainView
+import ru.vdv.myapp.dadapproves.presentation.interfaces.MainFragmentView
 
 class DadsOfficePresenter(
     private val schedulers: IMySchedulers,
+    private val jokesRepository: JokesRepository,
     private val router: Router
-) : MvpPresenter<MainView>() {
+) : MvpPresenter<MainFragmentView>() {
+    private val disposables = CompositeDisposable()
 
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         viewState.init()
+    }
+
+    override fun attachView(view: MainFragmentView?) {
+        super.attachView(view)
+        getApprovedAnecdotesCount()
+        getApprovedStoriesCount()
+        getApprovedPoemsCount()
+        getApprovedAphorismsCount()
+    }
+
+    private fun getApprovedAphorismsCount() {
+        disposables += Single.zip(
+            jokesRepository.getCountApprovedJokesByCategoryId(Group.APHORISMS.id),
+            jokesRepository.getCountByCategoryId(Group.APHORISMS.id),
+            fun(t1, t2): String { return " $t1 из $t2" })
+            .subscribeOn(schedulers.io())
+            .subscribe(
+                { viewState.setAphorismsCount("Одобрено $it афоризмов") },
+                { onRepositoryRequestError(it) })
+    }
+
+    private fun getApprovedPoemsCount() {
+        disposables += Single.zip(
+            jokesRepository.getCountApprovedJokesByCategoryId(Group.POEMS.id),
+            jokesRepository.getCountByCategoryId(Group.POEMS.id),
+            fun(t1, t2): String { return " $t1 из $t2" })
+            .subscribeOn(schedulers.io())
+            .subscribe(
+                { viewState.setPoemsCount("Одобрено $it стихотворений") },
+                { onRepositoryRequestError(it) })
+    }
+
+    private fun getApprovedStoriesCount() {
+        disposables += Single.zip(
+            jokesRepository.getCountApprovedJokesByCategoryId(Group.STORIES.id),
+            jokesRepository.getCountByCategoryId(Group.STORIES.id),
+            fun(t1, t2): String { return " $t1 из $t2" })
+            .subscribeOn(schedulers.io())
+            .subscribe(
+                { viewState.setStoriesCount("Одобрено $it рассказов") },
+                { onRepositoryRequestError(it) })
+    }
+
+    private fun getApprovedAnecdotesCount() {
+        disposables += Single.zip(
+            jokesRepository.getCountApprovedJokesByCategoryId(Group.ANECDOTES.id),
+            jokesRepository.getCountByCategoryId(Group.ANECDOTES.id),
+            fun(t1, t2): String { return " $t1 из $t2" })
+            .subscribeOn(schedulers.io())
+            .subscribe(
+                { viewState.setAnecdotesCount("Одобрено $it анекдотов") },
+                { onRepositoryRequestError(it) })
+    }
+
+    private fun onRepositoryRequestError(e: Throwable) {
+        Log.d("Моя проверка", "поток вернул ошибку: $e")
+
     }
 
     fun goToContentView(i: Int) {
@@ -23,5 +88,9 @@ class DadsOfficePresenter(
     fun backPressed(): Boolean {
         router.backTo(AndroidScreens().main())
         return true
+    }
+
+    override fun onDestroy() {
+        disposables.clear()
     }
 }
